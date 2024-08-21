8b9a40e06b1a60721478f978dcb13a3c2126b22e
IdEvEbI <idevebi@163.com>
2024-08-21 12:51:13 +0800

    create react spa project with vite

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    // 模拟从 API 获取数据
+    const fetchUserData = async () => {
+      const userData = {
+        name: '张三',
+        age: 30,
+      }
+      setUser(userData)
+    }
+    fetchUserData()
+  }, [])
+
+  if (!user) {
+    return <div>加载数据中...</div>
+  }
+
+  return (
+    <>
+      <h1>用户信息</h1>
+      <div>姓名：{user.name}</div>
+      <div>年龄：{user.age}</div>
+    </>
+  )
+}
+
+export default App
